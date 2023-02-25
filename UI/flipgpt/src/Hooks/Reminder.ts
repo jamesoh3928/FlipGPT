2432e90f858954e61581e8665d9418bccca83005
Ben Sippel <brs6412@rit.edu>
2023-02-25 15:21:32 -0500

    Initial notification function implemented

--- UI/flipgpt/src/Hooks/Reminder.ts
+++ UI/flipgpt/src/Hooks/Reminder.ts
@@ -1,2 +1,23 @@
 import { Twilio } from 'twilio';
 import React, { useState, useEffect } from 'react';
+import { notDeepStrictEqual } from 'assert';
+
+const accountSid = 'ACf6ba8eb8b094cb279e8e1405ee418b9e';
+const authToken = 'b1490109ffb1985a68efc8b3ff930376';
+const twilioNumber = '18339312987';
+
+export function sendNotif(phoneNumber: string, notification: string){
+    if(phoneNumber){
+        const client = new Twilio(accountSid, authToken);
+
+        client.messages
+            .create({
+                from: twilioNumber,
+                to: phoneNumber,
+                body: notification
+            })
+            .then((message) => console.log(message.sid));
+    }else{
+        console.error("Missing phone number to send notifcation to.");
+    }
+}
