46308ec7855b5b703950930c20b7d5b5dcdb3528
Colin Tondreau <colin.d.m.tondreau@gmail.com>
2023-02-25 14:58:32 -0500

    login page ready for inputs

--- UI/flipgpt/src/Screens/Login.tsx
+++ UI/flipgpt/src/Screens/Login.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login() {
+  const [userName, setUserName] = useState<String>("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="flex flex-center flex-col">
-      <h1 className="fs-45 f-white">Getting Started</h1>
-      <p className="fs-20 f-white">
-        Login or <span>Create Account</span>
-      </p>
+      <div className="flex flex-center flex-col" style={{ marginTop: 20 }}>
+        <h1 className="margin-0 f-bolder fs-45 f-white">Getting Started</h1>
+        <p className="margin-10 f-bold fs-20 f-white">
+          Login or <span className="f-orange">Create Account</span>
+        </p>
+      </div>
 
       <div className="input-boxes"></div>
     </div>
