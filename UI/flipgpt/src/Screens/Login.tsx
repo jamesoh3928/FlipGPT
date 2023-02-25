97cfbb652dfdbea3125fcf93a41260d00ebc7b3a
Colin Tondreau <colin.d.m.tondreau@gmail.com>
2023-02-25 14:48:46 -0500

    global styles

--- UI/flipgpt/src/Screens/Login.tsx
+++ UI/flipgpt/src/Screens/Login.tsx
@@ -2,8 +2,13 @@ import React from "react";
 
 function Login() {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="flex flex-center flex-col">
+      <h1 className="fs-45 f-white">Getting Started</h1>
+      <p className="fs-20 f-white">
+        Login or <span>Create Account</span>
+      </p>
+
+      <div className="input-boxes"></div>
     </div>
   );
 }
